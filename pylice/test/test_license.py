fb1a62948791a93e9de75beb10eb5380a4ec0518
Thomas Prebble <thomas.prebble@gmail.com>
2014-12-30 04:30:08 +1300

    Updating naming

--- pylice/test/test_license.py
+++ pylice/test/test_license.py
@@ -1,19 +1,18 @@
 import unittest
 
-from pylice import get_license_info_for_distribution
-from pylice import get_license_info_for_working_set
+from pylice import license
 
 
-class TestLicenseInfo(unittest.TestCase):
+class TestLicense(unittest.TestCase):
 
     def test_get_license_info_for_distribution(self):
-        self.assertRaises(Exception, get_license_info_for_distribution, "TEST")
+        self.assertRaises(Exception, license.get_license_info_for_distribution, "TEST")
 
-        name, _license = get_license_info_for_distribution("setuptools")
+        name, _license = license.get_license_info_for_distribution("setuptools")
         # Check for setuptools (Python 2) or distribute (Python 3)
         self.assertTrue(name.startswith("setuptools") or name.startswith('distribute'))
         self.assertEqual(_license, "PSF or ZPL")  # Too optimistic?
 
     def test_get_license_info_for_working_set(self):
-        license_info = get_license_info_for_working_set()
+        license_info = license.get_license_info_for_working_set()
         self.assertGreater(len(license_info), 0)
